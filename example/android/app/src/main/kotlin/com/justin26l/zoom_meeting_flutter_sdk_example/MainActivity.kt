3daaae29a48271e43bd3d68fcab85800ea15c924
Justin Lai <laichimeng2001@gmail.com>
2025-03-18 12:17:14 +0800

    init example

---
+++ example/android/app/src/main/kotlin/com/justin26l/zoom_meeting_flutter_sdk_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.justin26l.zoom_meeting_flutter_sdk_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:zoom_native_sdk/zoom_meeting_flutter_sdk.dart';
+import 'package:zoom_native_sdk/zoom_native_sdk.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _zoomNativelyPlugin = ZoomMeetingFlutterSdk();
-  bool isInitialized = false;
+  final _zoomNativeSdkPlugin = ZoomNativeSdk();
 
   @override
   void initState() {
@@ -27,24 +26,21 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
-
     try {
-      if (!isInitialized) {
-        isInitialized = (await _zoomNativelyPlugin.initZoom(
-          jwtToken: "",
-          )) ??
-          false;
-      }
-    } on PlatformException catch (e) {
-      debugPrint(e.message);
+      // platformVersion =
+      //     await _zoomNativeSdkPlugin.getPlatformVersion() ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
     }
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
     if (!mounted) return;
+
   }
 
   @override
@@ -54,20 +50,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              debugPrint("joinMeting -> isInitialized = $isInitialized");
-              if (isInitialized) {
-                await _zoomNativelyPlugin.joinMeting(
-                  meetingNumber: "",
-                  meetingPassword: "",
-                  displayName: ""
-                );
-              }
-            },
-            child: const Text("join"),
-          ),
+        body: const Center(
+          child: Text('Running on: \n'),
         ),
       ),
     );
